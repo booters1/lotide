54f10471fca5db165d903bd4624d747081e013ff
Ryan Huynh <151612334+booters1@users.noreply.github.com>
2024-02-05 13:34:41 -0700

    Add files via upload

---
+++ counterLetters.js
@@ -0,0 +1,45 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅pass✅✅✅: ${actual} = ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑fail🛑🛑🛑: ${actual} != ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+
+  for (const letter of sentence) {
+    // if not empty
+    if (letter !== ' ') {
+      // if repeat add one
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else { // else initialize count to 1
+        letterCount[letter] = 1;
+      }
+    }
+  }
+
+  return letterCount;
+};
+
+
+// TEST CODE
+const runTest = function() {
+  const result = countLetters("Lighthouse Labs");
+  assertEqual(result['L'], 2);
+  assertEqual(result['i'], 1);
+  assertEqual(result['g'], 1);
+  assertEqual(result['h'], 2);
+  assertEqual(result['t'], 1);
+  assertEqual(result['o'], 1);
+  assertEqual(result['u'], 1);
+  assertEqual(result['s'], 1);
+  assertEqual(result['e'], 1);
+  assertEqual(result['a'], 1);
+  assertEqual(result['b'], 1);
+};
+
+runTest();
