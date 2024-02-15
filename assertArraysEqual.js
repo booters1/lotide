2439f08fe2fde8a23a09767fd061d5c91325988e
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-15 10:10:25 -0700

    completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  // if the length arrays are different
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // check inside array
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const message = eqArrays(actual, expected) ? '✅✅✅pass✅✅✅' : '🛑🛑🛑fail🛑🛑🛑';
+  console.log(`${message}: [${actual}] === [${expected}]`);
+};
+
+// Example usage:
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
