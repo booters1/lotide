60ee70db11324908214c044723a6bfaec522b92d
Ryan Huynh <151612334+booters1@users.noreply.github.com>
2024-02-14 17:02:53 -0700

    Add files via upload

---
+++ assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+const runTest = function() {
+  assertEqual("Lighthouse Labs", "Bootcamp");
+
+  assertEqual(1, 1);
+};
+
+runTest();
