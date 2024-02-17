d9cfaaedec722e2702402a49cf29c2a810baf11e
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-17 00:01:53 -0700

    Implement letterPositions function and add test code

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,51 @@
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+
+    //if letter is not space, check for it , if not exists create the array and add   it to the correspondence
+    if (letter !== ' ') {
+      if (!results[letter]) {
+        results[letter] = [];
+      }
+      results[letter].push(i);
+    }
+  }
   return results;
 };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅pass✅✅✅: ${actual} = ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑fail🛑🛑🛑: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // if the length arrays are different
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // check inside array
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const message = eqArrays(actual, expected) ? '✅✅✅pass✅✅✅' : '🛑🛑🛑fail🛑🛑🛑';
+  console.log(`${message}: [${actual}] === [${expected}]`);
+};
+
+
+// Test for the string "hello"
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
