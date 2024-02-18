77d21e6217beaf75636948d32e0eb6755daa8c03
Ryan Huynh <151612334+booters1@users.noreply.github.com>
2024-02-17 20:11:55 -0700

    Update middleTest.js w mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,27 @@
 
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-//TEST CODES
+
+describe('#middle', () => {
+  it('should return empty array with one or two elements', () => {
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('should return middle element for arrays odd length', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('should return two middle elements for arrays even length', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
+
+/*/TEST CODES
 // one/two element = no middle = return empty
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
@@ -13,4 +32,4 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
 // even number = two elements in the middle returned
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); */
