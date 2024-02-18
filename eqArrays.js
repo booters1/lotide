c797267fdd38881caa13b58449ab65e3f89afa1b
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-17 21:12:40 -0700

    added tests in comments

--- eqArrays.js
+++ eqArrays.js
@@ -24,3 +24,10 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
+
+/*
+test cases that arent in test folder
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => should return true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => should return false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => should return false
+*/ 
