0a795402fd455cda93145edd8c95c633b1945598
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-17 18:05:01 -0700

    takeUntil.js completed w test case

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    // cb to current element
+    if (callback(item)) {
+      // cb = true, stop if and return
+      return result;
+    }
+    // if cb = false , add element to array
+    result.push(item);
+  }
+  return result;
+};
+
+const eqArrays = function(array1, array2) {
+  // if the length arrays are different
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // check inside array
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const message = eqArrays(actual, expected) ? '✅✅✅pass✅✅✅' : '🛑🛑🛑fail🛑🛑🛑';
+  console.log(`${message}: [${actual}] === [${expected}]`);
+};
+
+//test caSES
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+/*
+Expected Output
+[ 1, 2, 5, 7, 2 ]
+--
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
